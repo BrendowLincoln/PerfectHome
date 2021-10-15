1b853b9210bdcd2cdcb5b4ac227f764a89b8ccc5
Brendow Souza <brendow.souza@sapiensia.com>
2021-10-15 16:11:23 -0300

    WIP: Chenge the way to do de carousel traaction

--- src/app/shared/corousel/corousel.component.ts
+++ src/app/shared/corousel/corousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-corousel',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./corousel.component.scss']
 })
 export class CorouselComponent implements OnInit {
+  
+  @Input()
+  public valuesArray: Array<number> = [];
 
   constructor() { }
 
   ngOnInit(): void {
+
+    
   }
 
 }
